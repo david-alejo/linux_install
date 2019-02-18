1ac1f6c5552d4cb500425105877e87e5284b9122
David Alejo Teissière <churr.te@gmail.com>
2019-02-18 12:39:54 +0100

    Update install_chur_packages.bash

--- scripts/install_chur_packages.bash
+++ scripts/install_chur_packages.bash
@@ -17,3 +17,7 @@ rm vs_code.deb
 
 # Slack
 wget -O slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.1.0-amd64.deb && sudo apt install ./slack.deb && rm slack.deb
+
+# Power saving and related source: http://tipsonubuntu.com/2018/11/18/quick-tip-improve-battery-life-ubuntu-18-04-higher/
+sudo add-apt-repository ppa:linuxuprising/apps
+sudo aptitude install tlp powertop tlpui
