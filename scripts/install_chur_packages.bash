bb477d71352f93117b707156830a76ff5c4393d3
David Alejo Teissière <churr.te@gmail.com>
2019-09-09 20:31:27 +0200

    Update install_chur_packages.bash

--- scripts/install_chur_packages.bash
+++ scripts/install_chur_packages.bash
@@ -14,8 +14,7 @@ sudo aptitude install -y kate kdevelop kompare kolourpaint4
 
 
 # Multimedia packages
-sudo aptitude install -y vlc
-sudo aptitude install .y kdenlive # Earlier to 18.04 it's better to install the app image https://kdenlive.org/en/download/
+sudo aptitude install -y vlc kdenlive # Earlier to 18.04 it's better to install the app image https://kdenlive.org/en/download/
 
 # visual code
 wget -O vs_code.deb https://go.microsoft.com/fwlink/?LinkID=760868 
