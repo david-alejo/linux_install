13555cd48ba4967de4e1e4456510e8e9466f5e4a
David Alejo Teissière <churr.te@gmail.com>
2022-05-12 14:01:02 +0200

    Update set_date.sh

--- scripts/set_date.sh
+++ scripts/set_date.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo date -s "$(wget --method=HEAD -qS0- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-8)"
+sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-8)"
