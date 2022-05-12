74fdc54e3cbe6b302a0867ab27d407d345d5a4f4
david-alejo <churr.te@gmail.com>
2022-05-12 14:59:16 +0200

    Tested in different machines

--- scripts/set_date.sh
+++ scripts/set_date.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-timezone="$(timedatectl | grep "Time zone:" | cut -d' ' -f19)"
+timezone="$(timedatectl | grep "Time zone:" | xargs | cut -d' ' -f3)"
 echo $timezone
 sudo timedatectl set-timezone Universal
-sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-8)"
+sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-8)" > /dev/null
 sudo timedatectl set-timezone $timezone
+date
