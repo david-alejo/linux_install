fe43be78b387ef946892b7bbc7caccdad30f2518
david-alejo <churr.te@gmail.com>
2022-05-12 14:45:57 +0200

    Set date takes into account the timezone

--- scripts/set_date.sh
+++ scripts/set_date.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+timezone="$(timedatectl | grep "Time zone:" | cut -d' ' -f19)"
+echo $timezone
+sudo timedatectl set-timezone Universal
 sudo date -s "$(wget --method=HEAD -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f4-8)"
+sudo timedatectl set-timezone $timezone
