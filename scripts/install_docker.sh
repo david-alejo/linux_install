483207774a421c683b8f26fd30f409eeca6775ad
david-alejo <churr.te@gmail.com>
2023-02-21 18:22:20 +0100

    Added docker installation script

---
+++ scripts/install_docker.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+sudo aptitude install docker docker-ce -y
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install nvidia-docker2 -y
+# Add the user to the docker group
+sudo usermod -aG docker $USER
+# Restart docker
+sudo systemctl restart docker.service
+
+# Test
+docker run --rm nvidia/cuda:11.1-cudnn8-runtime-ubi8 nvidia-smi
