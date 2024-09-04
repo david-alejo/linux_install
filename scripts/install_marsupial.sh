cb3fbf626524c5e7bdc5ee482b2e31c6592060e0
david-alejo <churr.te@gmail.com>
2024-09-04 14:12:29 +0200

    Omitted git part dockerfile

--- scripts/install_marsupial.sh
+++ scripts/install_marsupial.sh
@@ -14,10 +14,6 @@ cd ~/marsupial_ws/src
 
 source ~/marsupial_ws/devel/setup.bash
 
-# For localization stuff
-git clone -noetic https://github.com/robotics-upo/dll.git
-git clone https://github.com/robotics-upo/odom_to_tf.git
-
 ## Dependencies for Behavior Tree
 echo "\n Installing BT Dependencies \n\n"
 sudo apt-get install ros-$ROS_DISTRO-ros-type-introspection ros-$ROS_DISTRO-nmea-msgs ros-$ROS_DISTRO-mavlink ros-$ROS_DISTRO-geographic-msgs -y
@@ -25,44 +21,6 @@ sudo apt-get install ros-$ROS_DISTRO-mavros
 sudo apt-get install qtbase5-dev libqt5svg5-dev -y
 sudo apt install libdw-dev libann-dev -y
 sudo apt-get install ros-noetic-geographic-msgs ros-noetic-nmea-msgs
-
-echo "\n Installing BT Packages \n\n"
-# git clone https://github.com/robotics-upo/behavior-tree-upo-actions.git
-git clone https://github.com/robotics-upo/BehaviorTree.CPP.git
-git clone https://github.com/robotics-upo/Groot.git
-
-# To install planner lazy-theta*
-echo "\n Installing Lazy Theta* Planner \n\n"
-git clone -b marsupial https://github.com/robotics-upo/lazy_theta_star_planners.git
-
-# To install NIX_common package
-echo "\n Installing NIX common package \n\n"
-git clone https://github.com/robotics-upo/nix_common.git
-
-# To install Random planners
-echo "\n Installing Random Planner \n\n"
-git clone -b feat_uav_planner_maths https://github.com/SaimonMR/rrt_star_planners.git
-
-# To install Marsupial Optimizer
-echo "\n Installing Marsupial Optimizer \n\n"
-git clone -b noetic https://github.com/robotics-upo/marsupial_optimizer.git
-
-# To get action for actionlib
-echo "\n Installing UPO Actions \n\n"
-git clone -b marsupial https://github.com/robotics-upo/upo_actions.git
-
-#To get a marker in the desired frame_link
-echo "\n Installing UPO Markers \n\n"
-git clone https://github.com/robotics-upo/upo_markers.git
-
-# Get timed roslaunch
-git clone -b melodic-devel https://github.com/robotics-upo/timed_roslaunch.git
-
-# Get upo path tracker
-git clone -b simplified https://github.com/robotics-upo/upo_path_tracker.git
-
-# Get catenary checker
-git clone -b dt_new_maths  https://github.com/robotics-upo/catenary_checker.git
 
 sudo apt-get install libqt5charts5-dev libqt5widgets5
 cd ~
@@ -74,4 +32,51 @@ cmake ..
 make
 sudo make install
 cd ~/marsupial_ws/src
+
+if [ $1="no_git" ]; then
+   source="country"
+   samples="US Canada Mexico..."
+else
+    # For localization stuff
+    git clone -noetic https://github.com/robotics-upo/dll.git
+    git clone https://github.com/robotics-upo/odom_to_tf.git
+
+    echo "\n Installing BT Packages \n\n"
+    # git clone https://github.com/robotics-upo/behavior-tree-upo-actions.git
+    git clone https://github.com/robotics-upo/BehaviorTree.CPP.git
+    git clone https://github.com/robotics-upo/Groot.git
+
+    # To install planner lazy-theta*
+    echo "\n Installing Lazy Theta* Planner \n\n"
+    git clone -b marsupial https://github.com/robotics-upo/lazy_theta_star_planners.git
+
+    # To install NIX_common package
+    echo "\n Installing NIX common package \n\n"
+    git clone https://github.com/robotics-upo/nix_common.git
+
+    # To install Random planners
+    echo "\n Installing Random Planner \n\n"
+    git clone -b feat_uav_planner_maths https://github.com/SaimonMR/rrt_star_planners.git
+
+    # To install Marsupial Optimizer
+    echo "\n Installing Marsupial Optimizer \n\n"
+    git clone -b develop_chur https://github.com/robotics-upo/marsupial_optimizer.git
+
+    # To get action for actionlib
+    echo "\n Installing UPO Actions \n\n"
+    git clone -b marsupial https://github.com/robotics-upo/upo_actions.git
+
+    #To get a marker in the desired frame_link
+    echo "\n Installing UPO Markers \n\n"
+    git clone https://github.com/robotics-upo/upo_markers.git
+
+    # Get timed roslaunch
+    git clone -b melodic-devel https://github.com/robotics-upo/timed_roslaunch.git
+
+    # Get upo path tracker
+    git clone -b simplified https://github.com/robotics-upo/upo_path_tracker.git
+
+    # Get catenary checker
+    git clone -b dt_new_maths  https://github.com/robotics-upo/catenary_checker.git
+fi
 
